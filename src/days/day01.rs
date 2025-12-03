113b86fd75e122cb0ea9c8605d228530765b38f2
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-12-03 22:28:28 +0200

    day1 part2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,19 +1,22 @@
 use aoc2025::utils::day01_input;
 
 pub fn solve() {
-    let instructions = day01_input("inputs/day01test.txt");
+    let instructions = day01_input("inputs/day01.txt");
     let mut dial: i32 = 50;
-    let mut p2: i32 = 0;
-    for (dir, amount) in instructions {
-        dial = match dir.as_str() {
-            "L" => (dial - amount + 100) % 100,
-            "R" => (dial + amount) % 100,
-            _ => 0,
-        };
-        if dial == 0 {
-            p2 += 1;
+    let mut zeros: i32 = 0;
+    for (dir, mut num) in instructions {
+        if dir == "L" {
+            num = -num;
         }
-        println!("Dir: {dir}, Amount: {amount}, Dial: {dial}, p2: {p2}");
+        if dial == 0 && num < 0 {
+            zeros -= 1;
+        }
+        dial += num;
+        zeros += dial.div_euclid(100).abs();
+        dial = dial.rem_euclid(100);
+        if dial == 0 && num < 0 {
+            zeros += 1;
+        }
     }
-    println!("Day1 Part2: {p2}");
+    println!("Day1 Part2: {zeros}");
 }
---
+++ src/days/day02.rs
@@ -0,0 +1,5 @@
+use aoc2025::utils::file_to_vec;
+
+pub fn solve() -> i64 {
+    let input = file_to_vec("../../inputs/day01test.txt");
+}
