466970371d488204be74dc20f61f3180a18b39f7
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-12-06 01:18:31 +0200

    day3 part1

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,6 +1,49 @@
 use aoc2025::utils::file_to_vec;
 
 pub fn solve() {
-    let input: Vec<String> = file_to_vec("src/days/inputs/day03/day03test.txt");
+    let input: Vec<String> = file_to_vec("src/days/inputs/day03/day03.txt");
     println!("{:?}", input);
+    let mut total: u32 = 0;
+    for batteries in input {
+        let as_chars: Vec<char> = batteries.chars().collect();
+        let as_ints: Vec<u32> = as_chars
+            .into_iter()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+        let mut max_first: u32 = 0;
+        let mut max_first_idx: usize = 0;
+        for (i, max) in as_ints.iter().enumerate() {
+            if *max > max_first {
+                max_first = *max;
+                max_first_idx = i;
+            }
+        }
+        let mut max_second: u32 = 0;
+        let mut max_second_idx: usize = 0;
+        if max_first_idx == as_ints.len() - 1 {
+            for i in 0..as_ints.len() - 1 {
+                if as_ints[i] > max_second {
+                    max_second = as_ints[i];
+                    max_second_idx = i;
+                }
+            }
+        } else {
+            for i in max_first_idx + 1..as_ints.len() {
+                if as_ints[i] > max_second {
+                    max_second = as_ints[i];
+                    max_second_idx = i;
+                }
+            }
+        }
+        let mut right_str = max_first.to_string();
+        let mut left_str = max_second.to_string();
+        if max_first_idx < max_second_idx {
+            right_str.push_str(&left_str);
+            total += right_str.parse::<u32>().unwrap();
+        } else {
+            left_str.push_str(&right_str);
+            total += left_str.parse::<u32>().unwrap();
+        }
+    }
+    println!("Day03 P1: {total}");
 }
